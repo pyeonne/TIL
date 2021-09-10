356731be8cf1d9a63b59500347cf4750ac6d9bde
pyeonne <pyeonne@gmail.com>
2021-09-10 15:04:55 +0900

    📝 Change from promises to async

--- JS/js-tutorial/async/promise-to-async.js
+++ JS/js-tutorial/async/promise-to-async.js
@@ -1,30 +1,37 @@
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+'use strict';
+class UserStorage {
+    delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async loginUser(id, password) {
+        await this.delay(2000);
+        if ((id === 'ellie' && password === 'dream') || (id === 'coder' && password === 'academy')) {
+            return id;
+        } else {
+            throw 'not found';
+        }
+    }
 
-async function loginUser(id, password) {
-    await delay(2000);
-    if ((id === 'ellie' && password === 'dream') || (id === 'coder' && password === 'academy')) {
-        return id;
-    } else {
-        return new Error('not found');
+    async getRoles(user) {
+        await this.delay(1000);
+        if (user === 'ellie') {
+            return { name: 'ellie', role: 'admin' };
+        } else {
+            throw 'no access';
+        }
+    }
+    // 이거 추가함
+    async getUserWithRole(user, password) {
+        const id = await this.loginUser(user, password);
+        const role = await this.getRoles(id);
+        return role;
     }
 }
-
-async function getRoles(user) {
-    await delay(1000);
-    if (user === 'ellie') {
-        return { name: 'ellie', role: 'admin' };
-    } else {
-        return new Error('no access');
-    }
-}
-
+// promise chaining -> ✨ async/await ✨
+const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-loginUser(id, password)
-    .then(getRoles)
-    .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
-    .catch(console.log);
-
-// class UserStorage 안에 작성하면 왜 에러가 나는 걸까?
+userStorage
+    .getUserWithRole(id, password) //
+    .catch(console.log)
+    .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`));
